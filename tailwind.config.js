7ae89a1c90a0c15085be328e98445b90f85e98c9
trafficlunar <trafficlunar@proton.me>
2024-12-03 20:10:25 +0000

    fix: require is not defined in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import tailwindcssAnimate from 'tailwindcss-animate';
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	darkMode: ["class"],
@@ -12,5 +14,5 @@ export default {
 			colors: {},
 		},
 	},
-	plugins: [require("tailwindcss-animate")],
+	plugins: [tailwindcssAnimate],
 };
