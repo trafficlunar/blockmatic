8a3ccfc0245c80902d5b951dd9abcc60fa96a918
trafficlunar <trafficlunar@proton.me>
2024-12-14 19:45:38 +0000

    chore: add svgr plugin to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
-import path from 'path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src")
-    }
-  }
-})
+	plugins: [react(), svgr()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+});
