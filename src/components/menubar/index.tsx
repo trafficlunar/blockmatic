2da5ba7af31411b2c663ac66d74ad4921ace56fd
trafficlunar <trafficlunar@proton.me>
2024-12-14 23:11:20 +0000

    fix: replace blockmatic text with svg

--- src/components/menubar/index.tsx
+++ src/components/menubar/index.tsx
@@ -1,18 +1,21 @@
-import { Menubar as UIMenubar } from "@/components/ui/menubar";
+import { Link } from "react-router";
 
+import { Menubar as UIMenubar } from "@/components/ui/menubar";
 import { DialogProvider } from "@/context/DialogContext";
 
 import FileMenu from "./FileMenu";
 import ViewMenu from "./ViewMenu";
 import MoreMenu from "./MoreMenu";
 
+import BlockmaticText from "@/assets/blockmatic-text.svg?react";
+
 function Menubar() {
 	return (
 		<DialogProvider>
 			<UIMenubar className="rounded-none border-t-0 border-x-0 col-span-2">
-				<a href="https://github.com/trafficlunar/blockmatic" className="ml-4 mr-2">
-					blockmatic
-				</a>
+				<Link to={{ pathname: "/" }} className="px-4 w-32">
+					<BlockmaticText className="h-full w-full" />
+				</Link>
 
 				<FileMenu />
 				<ViewMenu />
