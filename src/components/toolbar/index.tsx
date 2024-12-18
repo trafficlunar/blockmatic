a2f03bbaf0d0529a96565a9c9c7854965d52cb89
trafficlunar <trafficlunar@proton.me>
2024-12-18 21:11:47 +0000

    feat: add tooltips to toolbar

--- src/components/toolbar/index.tsx
+++ src/components/toolbar/index.tsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { EraserIcon, HandIcon, PencilIcon, ZoomInIcon } from "lucide-react";
 
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 import { ToolContext } from "@/context/Tool";
 
 function Toolbar() {
@@ -10,25 +11,62 @@ function Toolbar() {
 	const onToolChange = (value: string) => setTool(value as Tool);
 
 	return (
-		<ToggleGroup
-			type="single"
-			value={tool}
-			onValueChange={onToolChange}
-			className="flex flex-col justify-start py-1 border-r border-zinc-200 dark:border-zinc-800 bg-white dark:bg-zinc-950"
-		>
-			<ToggleGroupItem value="hand" className="!p-0 !h-8 !min-w-8">
-				<HandIcon />
-			</ToggleGroupItem>
-			<ToggleGroupItem value="pencil" className="!p-0 !h-8 !min-w-8">
-				<PencilIcon />
-			</ToggleGroupItem>
-			<ToggleGroupItem value="eraser" className="!p-0 !h-8 !min-w-8">
-				<EraserIcon />
-			</ToggleGroupItem>
-			<ToggleGroupItem value="zoom" className="!p-0 !h-8 !min-w-8">
-				<ZoomInIcon />
-			</ToggleGroupItem>
-		</ToggleGroup>
+		<TooltipProvider>
+			<ToggleGroup
+				type="single"
+				value={tool}
+				onValueChange={onToolChange}
+				className="flex flex-col justify-start py-1 border-r border-zinc-200 dark:border-zinc-800 bg-white dark:bg-zinc-950"
+			>
+				{/* Hand */}
+				<Tooltip delayDuration={0}>
+					<TooltipTrigger>
+						<ToggleGroupItem value="hand" className="!p-0 !h-8 !min-w-8">
+							<HandIcon />
+						</ToggleGroupItem>
+					</TooltipTrigger>
+					<TooltipContent side="right" sideOffset={10}>
+						<p>Hand (1)</p>
+					</TooltipContent>
+				</Tooltip>
+
+				{/* Pencil */}
+				<Tooltip delayDuration={0}>
+					<TooltipTrigger>
+						<ToggleGroupItem value="pencil" className="!p-0 !h-8 !min-w-8">
+							<PencilIcon />
+						</ToggleGroupItem>
+					</TooltipTrigger>
+					<TooltipContent side="right" sideOffset={10}>
+						<p>Pencil (2)</p>
+					</TooltipContent>
+				</Tooltip>
+
+				{/* Eraser */}
+				<Tooltip delayDuration={0}>
+					<TooltipTrigger>
+						<ToggleGroupItem value="eraser" className="!p-0 !h-8 !min-w-8">
+							<EraserIcon />
+						</ToggleGroupItem>
+					</TooltipTrigger>
+					<TooltipContent side="right" sideOffset={10}>
+						<p>Eraser (3)</p>
+					</TooltipContent>
+				</Tooltip>
+
+				{/* Zoom */}
+				<Tooltip delayDuration={0}>
+					<TooltipTrigger>
+						<ToggleGroupItem value="zoom" className="!p-0 !h-8 !min-w-8">
+							<ZoomInIcon />
+						</ToggleGroupItem>
+					</TooltipTrigger>
+					<TooltipContent side="right" sideOffset={10}>
+						<p>Zoom (4)</p>
+					</TooltipContent>
+				</Tooltip>
+			</ToggleGroup>
+		</TooltipProvider>
 	);
 }
 
