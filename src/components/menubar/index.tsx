1acc50012ffd99b2a35567cf6ad60af5bf1e4cf2
trafficlunar <trafficlunar@proton.me>
2024-12-24 17:42:32 +0000

    fix: fill on blockmatic text svg

--- src/components/menubar/index.tsx
+++ src/components/menubar/index.tsx
@@ -15,7 +15,7 @@ function Menubar() {
 		<DialogProvider>
 			<UIMenubar className="rounded-none border-t-0 border-x-0 col-span-2">
 				<Link to={{ pathname: "/" }} className="px-4 w-32">
-					<BlockmaticText className="h-full w-full" />
+					<BlockmaticText className="h-full w-full" fill="white" />
 				</Link>
 
 				<FileMenu />
