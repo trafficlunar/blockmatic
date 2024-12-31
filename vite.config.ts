85825726d681d92a8f73d73b495ff12f3379ad7f
trafficlunar <trafficlunar@proton.me>
2024-12-31 22:20:01 +0000

    fix: 4761b73d - missing file from commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import { defineConfig } from "vite";
 
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
-import { imagetools } from "vite-imagetools";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), svgr(), imagetools()],
+	plugins: [react(), svgr(), ViteImageOptimizer()],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
