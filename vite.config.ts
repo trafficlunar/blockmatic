1fafe67d1ada0802122058f964c75c5a3d92fedc
trafficlunar <trafficlunar@proton.me>
2024-12-31 22:03:39 +0000

    fix: convert screenshots/images to webp

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import { defineConfig } from "vite";
 
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import { imagetools } from "vite-imagetools";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), svgr()],
+	plugins: [react(), svgr(), imagetools()],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
