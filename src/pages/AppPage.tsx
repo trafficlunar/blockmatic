cee8cb92e1fbd9007e4b2c8ac52f4cefd1007614
trafficlunar <hello@trafficlunar.net>
2025-01-25 15:03:44 +0000

    fix: main page broken due to dff77e7b

--- src/pages/AppPage.tsx
+++ src/pages/AppPage.tsx
@@ -18,7 +18,7 @@ function AppPage() {
 					<SettingsProvider>
 						<TexturesProvider>
 							<ToolProvider>
-								<main className="h-screen grid grid-rows-[2.5rem_minmax(0,1fr)] grid-cols-[2.5rem_minmax(0,1fr)_auto]">
+								<main className="overflow-y-hidden h-screen grid grid-rows-[2.5rem_minmax(0,1fr)] grid-cols-[2.5rem_minmax(0,1fr)_auto]">
 									<Menubar />
 									<Toolbar />
 									<Canvas />
