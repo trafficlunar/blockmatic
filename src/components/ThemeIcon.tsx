44609671a9a1c7daa675538f21835fcc3a3a1e29
trafficlunar <hello@trafficlunar.net>
2025-02-08 17:39:10 +0000

    fix: change theme icon color based on theme

--- src/components/ThemeIcon.tsx
+++ src/components/ThemeIcon.tsx
@@ -7,7 +7,7 @@ interface Props {
 }
 
 function ThemeIcon({ inApp }: Props) {
-	const { theme, setTheme } = useContext(ThemeContext);
+	const { theme, setTheme, isDark } = useContext(ThemeContext);
 
 	const onClick = () => {
 		const nextTheme = theme === "light" ? "dark" : theme === "dark" ? "system" : "light";
@@ -16,14 +16,15 @@ function ThemeIcon({ inApp }: Props) {
 
 	const getIcon = () => {
 		const size = inApp ? 24 : 30;
+		const color = inApp ? (isDark ? "white" : "black") : "white";
 
 		switch (theme) {
 			case "light":
-				return <SunIcon size={size} />;
+				return <SunIcon size={size} color={color} />;
 			case "dark":
-				return <MoonIcon size={size} />;
+				return <MoonIcon size={size} color={color} />;
 			case "system":
-				return <SunMoonIcon size={size} />;
+				return <SunMoonIcon size={size} color={color} />;
 		}
 	};
 
